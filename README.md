1d6a0887f571d4e938f0b145d1df35d6fe331151
Glen Baker <iepathos@gmail.com>
2023-10-31 05:26:06 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# aistr
+
+Simple command line tool for interacting with openai chat-gpt.
+
+```sh
+aistr "what is a good name for a command line chat gpt tool?"
+
+```
+
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use chatgpt::prelude::*;
+use serde::{Deserialize, Serialize};
+use serde_yaml::{self};
+use chatgpt::types::{CompletionResponse};
+use std::env;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    openai_api_key: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let f = std::fs::File::open("config.yml").expect("Could not open file.");
+    let config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
+    //println!("{:?}", scrape_config);
+
+    // Creating a new ChatGPT client.
+    // Note that it requires an API key, and uses
+    // tokens from your OpenAI API account balance.
+    let client = ChatGPT::new(config.openai_api_key)?;
+
+    // Sending a message and getting the completion
+    println!("Prompt: {}", args[1..args.len()].join(" "));
+    let response: CompletionResponse = client
+       .send_message(args[1..args.len()].join(" "))
+       .await?;
+
+    println!("Response: {}", response.message().content);
+
+    Ok(())
+}
