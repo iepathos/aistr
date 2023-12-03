f1adb678dfefedaa563202444f08c7b3c566e461
Glen Baker <iepathos@gmail.com>
2023-12-03 15:40:59 -0800

    reworking

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Simple command line tool for interacting with openai chat-gpt.
 
 ```sh
-aistr "what is a good name for a command line chat gpt tool?"
-ChatBotCLI
+aistr -p "what is a good name for a chat gpt CLI?"
+ChatBot CLI
 ```
 
 Requires a config.yml 
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,25 @@ use chatgpt::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
 use chatgpt::types::{CompletionResponse};
-use std::env;
+// use std::env;
+use clap::Parser;
+
+/// Simple CLI for OpenAI chat-gpt
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Prompt to ask chat-gpt
+    #[arg(short, long)]
+    prompt: String,
+
+    /// OpenAI API key to use
+    // #[arg(short, long)]
+    // key: String,
+
+    /// Path to config.yml for storing API key and other options
+    #[arg(short, long)]
+    config: String,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
@@ -11,9 +29,10 @@ struct Config {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    let f = std::fs::File::open("config.yml").expect("Could not open file.");
+    let f = std::fs::File::open(args.config).expect("Could not open file.");
     let config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
     //println!("{:?}", scrape_config);
 
@@ -24,8 +43,9 @@ async fn main() -> Result<()> {
 
     // Sending a message and getting the completion
     // println!("Prompt: {}", args[1..args.len()].join(" "));
+    // args[1..args.len()].join(" ")
     let response: CompletionResponse = client
-       .send_message(args[1..args.len()].join(" "))
+       .send_message(args.prompt)
        .await?;
 
     println!("{}", response.message().content);
